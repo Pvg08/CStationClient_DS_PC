c99df602353e224dc9be78bcc50e70b4439ded71
posedelov <posedelov@rusoft.ru>
2016-03-12 15:28:54 +0300

    расширенный тоновый сигнал

--- PC_CStation_Client/classes/clientActions/clientactiontone.h
+++ PC_CStation_Client/classes/clientActions/clientactiontone.h
@@ -21,8 +21,12 @@ private slots:
     void tonePeriod();
 
 private:
+    static unsigned int oct_freq[9][7];
+    static float diez_k;
+    static float bemol_k;
+
     bool prog_led_tone_control;
-    bool tone_state, tone_periodic;
+    bool tone_state, tone_periodic, tone_is_melody;
     unsigned long int tone_frequency, tone_period;
 
     FMOD::System *system;
@@ -31,11 +35,22 @@ private:
     QTimer *updateTimer;
     QTimer *periodicToneTimer;
 
+    QString melody;
+    int sub_level;
+    unsigned int melody_pos;
+    unsigned int melody_tempo;
+    quint8 melody_timer_counter;
+    quint8 melody_timer_counter_max;
+    unsigned long timer_counter;
+    unsigned long timer_counter_max;
+
     void ERRCHECK(FMOD_RESULT op_result);
     unsigned long readIntFromString(QString message, unsigned *pos);
     void tone();
     void notone();
     void setProgLed(bool state);
+    void toneMelodyAction();
+    void startMelodyTone(QString cmelody);
 };
 
 #endif // CLIENTACTIONTONE_H
